3e379f0c7d5ee3df14fdbd67a0ff2b7f8b646ba7
Gabriel Paez <gpaez1992@gmail.com>
2022-02-20 18:46:13 -0300

    entrega simulador

--- Simulador_Interactivo/js/archivojs.js
+++ Simulador_Interactivo/js/archivojs.js
@@ -4,12 +4,22 @@
         btnConvertir.addEventListener('click', ()=>
         {      
             let entrada = parseInt(document.getElementById('numEntrada').value); // leo número de entrada (en base 10)
-            let base = 2;
-            if(document.getElementById('octal').checked)
-                base = 8;
-            document.querySelector("#resultado").innerHTML = "";
-            document.querySelector("#resultado").innerHTML += " Resultado en base " + base + ":<br>";
-            cambioDeBase(entrada, base);
+            let entradaF = parseFloat(document.getElementById('numEntrada').value);
+            if(entradaF - entrada != 0)
+            {
+                alert("Error: NO ha ingresado un número entero");
+            }
+            else
+            {
+                let base = 2;
+                if(document.getElementById('octal').checked)
+                    base = 8;
+                document.querySelector("#resultado").innerHTML = "";
+                document.querySelector("#resultado").innerHTML += " Resultado en base " + base + ":<br>";
+                document.querySelector("#pSalida").innerHTML = "";
+                document.getElementById("pSalida").style.padding= "0"; 
+                cambioDeBase(entrada, base);
+            }
         });
     }
 )();
@@ -27,23 +37,19 @@ function cambioDeBase(numEntrada, divisor)
             alert("Error: debe ingresar un número entero mayor o igual que cero");
         }
         else
-            if((numEntrada - Math.trunc(numEntrada)) != 0)  // se chequea si se ingresa un número con decimales
+        {
+            let cociente, resto;
+            let cadenaSalida = "";
+            do
             {
-                alert("Error: NO ha ingresado un número entero");
-            }
-            else
-            {
-                let cociente, resto;
-                let cadenaSalida = "";
-                do
-                {
-                    cociente = Math.trunc(numEntrada / divisor); // división entera
-                    resto =  numEntrada - divisor * cociente;
-                    numEntrada = cociente;
-                    cadenaSalida = resto + cadenaSalida;
-                }   
-                while(cociente != 0);
-                //muestro lo convertido 
-                document.querySelector("#pSalida").innerHTML = cadenaSalida; 
-            }
+                cociente = Math.trunc(numEntrada / divisor); // división entera
+                resto =  numEntrada - divisor * cociente;
+                numEntrada = cociente;
+                cadenaSalida = resto + cadenaSalida;
+            }   
+            while(cociente != 0);
+            //muestro lo convertido 
+            document.getElementById("pSalida").style.padding= ".25rem .5rem";
+            document.querySelector("#pSalida").innerHTML = cadenaSalida; 
+        }
 }
